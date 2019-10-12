66823576f01c04ab32a342d7652c86ea926750a3
Enzo <enzocremella95@gmail.com>
2019-10-12 01:26:17 -0300

    Listo el responsive de home.php. Falta solo meterle el menu completo a las opciones del boton menu

--- html/header.php
+++ html/header.php
@@ -1,8 +1,20 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 <header>
   <nav class="header_menu">
     <ul class="lista_titulo">
       <li class="titulo"><a href="home.php">Lagard</a></li>
     </ul>
+    <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Menu ≡
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+        <button class="dropdown-item" type="button">VEHICULOS</button>
+        <button class="dropdown-item" type="button">ACCESORIOS</button>
+        <button class="dropdown-item" type="button">INGRESA</button>
+      </div>
+    </div>
     <ul class="lista_menu">
       <li><a href="#">VEHICULOS</a></li>
       <li><a href="#">ACCESORIOS</a></li>
