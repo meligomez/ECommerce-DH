85ccae237a8bab597859ac41bb44c8ca0c32a542
meligomez <melisagomez.m@gmail.com>
2019-10-13 01:52:09 -0300

    Login y más

--- html/login.php
+++ html/login.php
@@ -1,11 +1,63 @@
 <!DOCTYPE html>
 <?php
-IF($_POST){
-
+$errorUsuario = "";
+$errorNoExisteUsuario = "";
+$errorUserYPsw="";
+$errorContrasenia = "";
+//Inicio de validacion de datos de los input
+if($_POST)
+{
+  // creo una variable para ver si hay errores
+  $errores = false;
+  if($_POST["user"] == ""){
+    $errorUsuario = "Debe ingresar un nombre de usuario";
+    $errores = true;
+}else if($_POST["user"] != "" && $_POST["password"] != ""){
+  if(!validarUserYPsw($_POST["user"],$_POST["password"]))
+  {
+    $errores=true;
+  }
+  else{
+    echo "Entro aca";
+    loguearse($_POST["user"],$_POST["password"]);
+  }
 }
 
 
+  if($_POST["password"] == ""){
+      $errorContrasenia = "Debe ingresar su contraseña";
+      $errores = true;
+  }
 
+}
+//Creo una función para validar el usuario y la contraseña
+function validarUserYPsw($unUser,$unaPsw){
+  global $errorUserYPsw;
+  global $errorNoExisteUsuario;
+  $userCo;
+  //Recorro el json de usuarios y encontrar ese usuario y validar la psw 
+  $urlJsonUsuarios=file_get_contents("../json/usuarios.json");
+  $usuarios =json_decode($urlJsonUsuarios,true);
+  foreach ($usuarios as $unUsuario ) {
+    if($unUsuario["usuario"]==$unUser){
+      if(password_verify($unaPsw,$unUsuario["contrasenia"])){
+        return true;
+      }
+      $errorUserYPsw="No coincide el usuario y la contraseña, valide los datos ingresados";
+      return false;
+    }
+    $errores=true;
+  }
+  $errorNoExisteUsuario="No existe el usuario indicado, valide los datos ingresados";
+  return false;
+}
+function loguearse($unUser,$unaPsw)  
+  {
+//crear la sesión / cookie y redirigirlo al home, 
+   session_start();
+    header('Location: /ECommerce-DH/html/home.php');
+
+  }
 
  ?>
 <html lang="es" dir="ltr">
@@ -16,7 +68,7 @@ IF($_POST){
     <link rel="stylesheet" href="../css/styles.css">
     <link href="https://fonts.googleapis.com/css?family=Karla&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Dosis|Great+Vibes&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Karla&display=swap" rel="stylesheet">
+     <link href="https://fonts.googleapis.com/css?family=Karla&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <?php include_once("header.php") ?>
@@ -29,12 +81,16 @@ IF($_POST){
         <h1>Sign In</h1>
         <div class="col-lg-12 nombre">
            <img src="../img/user.svg" alt="" width="6%">
-          <input class="inputNombreLogin" type="text" name="nombre" value="" placeholder="Username">
+          <input  <?php echo $errorUsuario!=""?"class='inputValidar'":""; ?> class="inputNombreLogin" type="text" name="user" <?php echo $errorUserYPsw!=""?" value='".$_POST["user"]."' ":""; ?> placeholder="Username">
+          <span style="color:red;font-size:12px;display:block;"><?=$errorUsuario;?></span>
         </div>
         <div class="password">
           <img src="../img/lock.svg" alt="" width="6%">
-          <input class="inputPassLogin" type="password" name="password" value="" placeholder="Password"  >
+          <input <?php echo $errorContrasenia!=""?"class='inputValidar'":""; ?> class="inputPassLogin" type="password" name="password" value="" placeholder="Password"  >
+          <span style="color:red;font-size:12px;display:block;"><?=$errorContrasenia;?></span>
         </div>
+        <span style="color:red;font-size:12px;"><?=$errorNoExisteUsuario;?></span>
+        <span style="color:red;font-size:12px;"><?=$errorUserYPsw;?></span>
         <div class="col-12 rememberMe">
             <label class="labelrememberMe"><input type="checkbox" id="cbox1" value="first_checkbox"> Recordarme</label>
         </div>
--- html/registroDeUsuarios.php
+++ html/registroDeUsuarios.php
@@ -4,7 +4,9 @@ $errorUsuario = "";
 $errorNombreYApellido = "";
 $errorEmail = "";
 $errorContrasenia = "";
-
+$errorContrasenias="";
+$errorUsuarioYaExiste="";
+global $usuarios;
 //Inicio de validacion de datos de los input
 if($_POST)
 {
@@ -12,14 +14,14 @@ if($_POST)
   $errores = false;
 
    if($_POST["usuario"] == ""){
-      $errorUsuario = "Ingrese un nombre de usuario";
+      $errorUsuario = "Debe ingresar un nombre de usuario";
       $errores = true;
-  }else if(strlen($_POST["usuario"]) < 2){
-      $errorUsuario = "Su nombre de usuario debe tener al menos 2 caracteres";
+  }else if(strlen($_POST["usuario"]) < 5){
+      $errorUsuario = "Su nombre de usuario debe tener al menos 5 caracteres";
       $errores = true;
   }
   if($_POST["nombreYapellido"] == ""){
-    $errorNombreYApellido = "Ingrese un nombre y apellido";
+    $errorNombreYApellido = "Debe ingresar un nombre y apellido";
     $errores = true;
     }else if(strlen($_POST["nombreYapellido"]) < 4){
       $errorNombreYApellido = "Su nombre y apellido deben tener al menos 4 caracteres";
@@ -33,10 +35,10 @@ if($_POST)
       $errores = true;
   }
   if($_POST["contrasenia"] == "" || $_POST["contraseniaConfirma"] == ""){
-      $errorContrasenia = "No puede estar en blanco";
+      $errorContrasenia = "Debe ingresar una contraseña";
       $errores = true;
   }else if($_POST["contrasenia"] != $_POST["contraseniaConfirma"]){
-      $errorContrasenia = "Las contrasenias deben coincidir";
+      $errorContrasenias = "Las contraseñas deben coincidir";
       $errores = true;
   }else{
       $contrasenia = password_hash($_POST["contrasenia"],PASSWORD_DEFAULT);
@@ -48,7 +50,19 @@ if($_POST)
 //primero valido si no hay ningun error
 if(!$errores)
 {
-//nos guardarmos los datos del post en un array
+//traigo los usuarios del json
+
+$usuariosEnJSON = file_get_contents("..\json\usuarios.json");
+
+//convierto el json en array
+$usuarios = json_decode($usuariosEnJSON);
+//agrego el nuevo usuario al array de la base de datos
+//validar que el usuario no exista
+
+if(existeUsuario($_POST["usuario"])){
+$errorUsuarioYaExiste="El nombre de usuario ya está en uso, por favor ingrese otro";
+}else{
+  //nos guardarmos los datos del post en un array
 $usuario=[
   "id"=> md5($_POST["usuario"]),
   "usuario" => $_POST["usuario"],
@@ -57,26 +71,33 @@ $usuario=[
   "fotoPerfil"=>$_POST["fotoPerfil"],
   "contrasenia" =>  $contrasenia
 ];
-//traigo los usuarios del json
-
-$usuariosEnJSON = file_get_contents("..\json\usuarios.json");
-
-//convierto el json en array
-$usuarios = json_decode($usuariosEnJSON);
-//agrego el nuevo usuario al array de la base de datos
 $usuarios[] = $usuario;
+}
+
 //convierto el nuevo array completo a json
 $nuevosUsuariosEnJSON = json_encode($usuarios);
 
 //escribo el nuevo json en el archivo .json
 file_put_contents("..\json\usuarios.json",$nuevosUsuariosEnJSON);
-echo "alert('Se registró correctamente')";
-exit;
+header('Location: ./login.php');
+
 }
   
   
 }
 
+
+function existeUsuario($userABuscar){
+  var_dump($usuarios);
+  exit;
+  foreach ($usuarios as $unUsuario) {
+    if($unUsuario["usuario"]==$userABuscar)
+    {
+      return true;
+    }
+  }
+  return false;
+}
 
  ?>
 <html>
@@ -103,14 +124,18 @@ exit;
             <p>
                 <img src="../img/user.svg" alt="" width="6%">
                 <input  <?php echo $errorUsuario!=""?"class='inputValidar'":""; ?>  id="usuario" class="inputEspacio" type="text" name="usuario" placeholder="Ingrese un Usuario" value="">
+                <span style="color:red;font-size:12px;display:block;"><?=$errorUsuario;?></span>
+                <span style="color:red;font-size:12px;display:block;"><?=$errorUsuarioYaExiste;?></span>
               </p>
             <p>
               <img src="../img/card.svg" alt="" width="6%">
               <input  <?php echo $errorNombreYApellido!=""?"class='inputValidar'":""; ?> id="nombreYapellido" class="inputEspacio" type="text" name="nombreYapellido" placeholder="Ingrese su nombre y apellido" value="">
+              <span style="color:red;font-size:12px;display:block;"><?=$errorNombreYApellido;?></span>
             </p>
             <p>
               <img src="../img/email.svg" alt="" width="6%">
               <input <?php echo $errorEmail!=""?"class='inputValidar'":""; ?> id="email" class="inputEspacio" type="email" name="email" placeholder="Ingrese su mail" value="">
+              <span style="color:red;font-size:12px;display:block;"><?=$errorEmail;?></span>
             </p>
             <p>
             <input type="file" name="fotoPerfil" placeholder="Ingrese su foto de perfil">
@@ -118,10 +143,12 @@ exit;
             <p>
               <img src="../img/lock.svg" alt="" width="6%">
               <input <?php echo $errorContrasenia!=""?"class='inputValidar''":""; ?> id="contrasenia" class="inputEspacio"  type="password" name="contrasenia" placeholder="Ingrese una contraseña" value="">
+              <span style="color:red;font-size:12px;display:block;"><?=$errorContrasenia;?></span>
             </p>
             <p>
               <img src="../img/lock.svg" alt="" width="6%">
               <input <?php echo $errorContrasenia!=""?"class='inputValidar'":""; ?> id="contraseniaConfirma" class="inputEspacio"  type="password" name="contraseniaConfirma" placeholder="Reingrese la contraseña" value="">
+              <span style="color:red;font-size:12px;display:block;"><?=$errorContrasenias;?></span>
             </p>
             <input class="inputRegistrar" type="submit" value="Registrarme"/>
          </form>  
